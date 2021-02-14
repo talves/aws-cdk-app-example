31fdfc853d2fed7a1e45cdce5b06efbc4e23f2d6
talves <tony@3alves.com>
2021-02-14 13:41:17 -0800

    missing ,

--- lambda/hello.js
+++ lambda/hello.js
@@ -1,9 +1,10 @@
-exports.handler = async function(event) {
-  console.log("event:" JSON.stringify(event, null, 2));
+"use strict";
+exports.handler = async function (event) {
+  console.log("event:", JSON.stringify(event, null, 2));
 
   return {
     statusCode: 200,
-    headers: {"Content-Type": "text/plain"},
-    body: "Hello, Everyone!"
-  }
-}
+    headers: { "Content-Type": "text/plain" },
+    body: "Hello, Everyone!",
+  };
+};
